f096921c3238c887aa480ad3b2d41909d37f25ca
Manav Patil <manavspatil2002@gmail.com>
2025-05-30 17:30:12 +0530

    code change

--- app/category/[categorySlug]/page.js
+++ app/category/[categorySlug]/page.js
@@ -1,17 +1,13 @@
-import fs from "fs/promises";
+import quizData from "@/app/data/frontendQuiz.json";
 import QuizClient from "@/app/Components/Quiz/quiz";
 
-const CategorySlugPage = async ({ params }) => {
+const CategorySlugPage = ({ params }) => {
   const { categorySlug } = params;
 
-  const response = await fs.readFile("frontendQuiz.db", "utf-8");
-  const data = JSON.parse(response);
-
-  const filtered = data.filter((item) => item.category === categorySlug);
-
+  const filtered = quizData.filter((item) => item.category === categorySlug);
   const shuffled = filtered.sort(() => 0.5 - Math.random()).slice(0, 10);
 
-  return <QuizClient category={categorySlug} questions={shuffled}/>;
+  return <QuizClient category={categorySlug} questions={shuffled} />;
 };
 
 export default CategorySlugPage;
